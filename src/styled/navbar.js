c1b6c066497712cab4e209b4f8f4123d2c560b77
Arai Shinya <>
2019-03-23 19:06:37 +0900

    navbar

---
+++ src/styled/navbar.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const NavbarContainer = styled.div`
+  width: 100%;
+  background-color: dimgray;
+`;
--- src/styled/sidenav.js
+++ src/styled/sidenav.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
 export const SideNavContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
+  height: 100vh;
   width: 20%;
+  background-color: dimgray;
 `;
 
 export const Title = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 5px;
+  height: 10rem;
 `;
