eeab29aba9e470728dd19281a68a7950eaeb9ac4
新井辰弥 <araishinya@iMac.local>
2019-03-29 19:03:49 +0900

    デザイン修正とファイル整理

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -4,29 +4,27 @@ import { exclamationTriangle } from 'react-icons-kit/fa/exclamationTriangle';
 
 import { ErrorWrapper } from '../../styled/NotFoundPage';
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { history } = this.props;
+const NotFoundPage = props => { 
+  const { history } = props;
 
-    return (
-      <ErrorWrapper>
-        <Icon size={100} icon={exclamationTriangle} />
-        <div 
-          className="ui animated fade button" 
-          tabIndex="0" 
-          style={{ marginTop: '2rem' }}
-          onClick={() => history.push('/')}
-        >
-          <div className="visible content">
-            トップページへ戻る
-          </div>
-          <div className="hidden content">
-            go back!!
-          </div>
+  return (
+    <ErrorWrapper>
+      <Icon size={100} icon={exclamationTriangle} />
+      <div 
+        className="ui animated fade button" 
+        tabIndex="0" 
+        style={{ marginTop: '2rem' }}
+        onClick={() => history.push('/')}
+      >
+        <div className="visible content">
+          トップページへ戻る
         </div>
-      </ErrorWrapper>
-    );
-  }
+        <div className="hidden content">
+          go back!!
+        </div>
+      </div>
+    </ErrorWrapper>
+  );
 };
 
 export default NotFoundPage;
