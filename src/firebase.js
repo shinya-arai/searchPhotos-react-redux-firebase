bcb44230f10f18b3142c54e32f40a165eedcf23b
Arai Shinya <>
2019-05-28 23:46:27 +0900

    .envから値取れなくなった、、

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,24 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+//   authDomain: "searchphotos-react.firebaseapp.com",
+//   databaseURL: "https://searchphotos-react.firebaseio.com",
+//   projectId: "searchphotos-react",
+//   storageBucket: "searchphotos-react.appspot.com",
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTH5rzooorY2s1rrmwUa_gcO9LMZ-UD6s",
   authDomain: "searchphotos-react.firebaseapp.com",
   databaseURL: "https://searchphotos-react.firebaseio.com",
   projectId: "searchphotos-react",
   storageBucket: "searchphotos-react.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APPID
+  messagingSenderId: "1053865513986",
+  appId: "1:1053865513986:web:4adda163b03c6768"
 };
 
 firebase.initializeApp(firebaseConfig);
